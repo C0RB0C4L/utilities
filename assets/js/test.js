31a21eb97f1803d3483d86000d8184dbec9a0274
cmaleix <79254830+C0RB0C4L@users.noreply.github.com>
2021-11-02 22:30:42 +0100

    tests

---
+++ assets/js/test.js
@@ -0,0 +1,37 @@
+console.log('oui');
+
+let $ = jQuery;
+
+
+toggleTooltipOnClick();
+closeTooltipsOnDOMClick();
+
+
+function toggleTooltipOnClick() {
+  $('.tooltip').click(function() {
+    let tooltipBox = $(this).next('.tooltip-container');    
+    if(tooltipBox.hasClass('visible')) {
+      tooltipBox.removeClass('visible');
+    } else {
+      closeEachTooltip();
+      tooltipBox.addClass('visible');
+    }
+  })
+}
+
+function closeTooltipsOnDOMClick() {
+  $(document).click(function(e) {
+    if($(e.target).hasClass('tooltip') || $(e.target).hasClass('tooltip-container') || $(e.target).parents().hasClass('tooltip-container')) {
+    } else {
+      closeEachTooltip();
+    }
+  })
+}
+
+
+function closeEachTooltip() {
+  let tooltips = $('.tooltip-container');
+  tooltips.each(function() {
+    $(this).removeClass('visible');
+  })
+}
